3099a9cdaadb741a5da75627ef169a377dc2826c
soulDearBoy <ro.diggle622@gmail.com>
2025-10-17 20:03:56 -0300

    SubTotal

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -23,104 +23,94 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T' HH:mm:ss'Z'", timezone = "GMT")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T' HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-private Integer orderStatus;
-	 
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
-	
+
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment ;
-	public Order() {
-		
-	}
 
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+
+	public Order() {
+
+	}
 
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
-	    super();
-	    this.id = id;
-	    this.moment = moment;
-	    this.client = client;
-	    setOrderStatus(orderStatus);  
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.client = client;
+		setOrderStatus(orderStatus);
 	}
 
-
-
-
-	
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public Instant getMoment() {
 		return moment;
 	}
 
-
-	public void setMoment(Instant moment) {	
+	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
+
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
 
-
 	public void setOrderStatus(OrderStatus orderStatus) {
-		if(orderStatus != null) {
-		this.orderStatus = orderStatus.getCode();
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
 		}
 	}
-
-
 
 	public User getClient() {
 		return client;
 	}
 
-
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
-	
-	
-public Payment getPayment() {
+
+	public Payment getPayment() {
 		return payment;
 	}
-
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
 
-
-public Set<OrderItem> getItems(){
-	return items;
-}
-
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
+	public Set<OrderItem> getItems() {
+		return items;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
+	@Override public int hashCode() {
+		return Objects.hash(id);
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -133,6 +123,5 @@ public Set<OrderItem> getItems(){
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
--- src/main/java/com/example/demo/entities/OrderItem.java
+++ src/main/java/com/example/demo/entities/OrderItem.java
@@ -68,6 +68,10 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		return price * quantities;
+	}
 
 	@Override
 	public int hashCode() {
